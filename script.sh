e2dcbaf5e233c14e85fa5d9bb2f5779351323601
Daniel Takeshi <martins.takeshi@gmail.com>
2023-01-04 11:39:31 -0300

    Add Project Files

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Criando as imagens"
+
+docker build -t danitakeshi/projeto-backend:1.0 backend/.
+docker build -t danitakeshi/projeto-database:1.0 database/.
+
+echo "Realizando Push das imagens"
+
+docker push danitakeshi/projeto-backend:1.0
+docker push danitakeshi/projeto-database:1.0
+
+echo "Criando Servicos no cluster Kubernetes"
+
+kubectl apply -f ./services.yml
+
+echo "Criando os Deployments"
+
+kubectl apply -f ./deployment.yml
